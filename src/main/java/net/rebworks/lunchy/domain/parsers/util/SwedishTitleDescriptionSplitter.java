3dabf32835701f7fcad2b8cfab453ac379dfa4db
R. Emre Basar <rebasar+git@rebworks.net>
2018-02-18 18:46:41 +0100

    Refactor text splitting. Closes: #12

--- src/main/java/net/rebworks/lunchy/domain/parsers/util/SwedishTitleDescriptionSplitter.java
+++ src/main/java/net/rebworks/lunchy/domain/parsers/util/SwedishTitleDescriptionSplitter.java
@@ -6,17 +6,18 @@ import java.util.regex.Pattern;
 public class SwedishTitleDescriptionSplitter {
 
     public static class TitleAndDescription {
+        private final String prefix;
         private final String title;
         private final String description;
 
-        private TitleAndDescription(final String title, final String description) {
+        private TitleAndDescription(final String prefix, final String title, final String description) {
+            this.prefix = prefix;
             this.title = title;
             this.description = description;
         }
 
-        private TitleAndDescription(final String title) {
-            this.title = title;
-            description = null;
+        private TitleAndDescription(final String prefix, final String title) {
+            this(prefix, title, null);
         }
 
         public String getTitle() {
@@ -27,25 +28,45 @@ public class SwedishTitleDescriptionSplitter {
             return Optional.ofNullable(description);
         }
 
-        public static TitleAndDescription from(final String[] split) {
+        public static TitleAndDescription from(final String prefix, final String[] split) {
             if (split.length == 1) {
-                return new TitleAndDescription(split[0].trim());
+                return new TitleAndDescription(prefix, split[0].trim());
             }
-            return new TitleAndDescription(split[0].trim(), split[1].trim());
+            return new TitleAndDescription(prefix, split[0].trim(), split[1].trim());
+        }
+
+    }
+
+    private static class Splitter {
+        private final String patternText;
+        private final Pattern pattern;
+
+        private Splitter(final String patternText) {
+            this.patternText = patternText;
+            this.pattern = Pattern.compile(patternText);
+        }
+
+        public TitleAndDescription split(final String title){
+            final String[] split = pattern.split(title, 2);
+            final String prefix = patternText.trim(); // TODO: Capitalise
+            return TitleAndDescription.from(prefix, split);
         }
     }
 
-    private static final Pattern MED = Pattern.compile(" med ", Pattern.CASE_INSENSITIVE);
-    private static final Pattern SERVERAS_MED = Pattern.compile(" serveras med ", Pattern.CASE_INSENSITIVE);
+    private static final String MED_PATTERN = " med ";
+    private static final String SERVERAS_MED_PATTERN = " serveras med ";
+    private static final Splitter MED_SPLITTER = new Splitter(MED_PATTERN);
+    private static final Splitter SERVERAS_MED_SPLITTER = new Splitter(SERVERAS_MED_PATTERN);
 
     public TitleAndDescription split(final String title) {
-        final String[] medSplit = MED.split(title, 2);
-        final String[] serverasMedSplit = SERVERAS_MED.split(title, 2);
-        if (medSplit.length > serverasMedSplit.length) return TitleAndDescription.from(medSplit);
-        if (serverasMedSplit.length > medSplit.length) return TitleAndDescription.from(serverasMedSplit);
-        if (medSplit[0].length() < serverasMedSplit[0].length()) return TitleAndDescription.from(medSplit);
-        if (serverasMedSplit[0].length() < medSplit[0].length()) return TitleAndDescription.from(serverasMedSplit);
-        return TitleAndDescription.from(medSplit);
+
+        final TitleAndDescription med = MED_SPLITTER.split(title);
+        final TitleAndDescription serverasMed = SERVERAS_MED_SPLITTER.split(title);
+
+        if (med.getDescription().isPresent() && !serverasMed.getDescription().isPresent()) return med;
+        if (serverasMed.getDescription().isPresent() && !med.getDescription().isPresent()) return serverasMed;
+        if (med.getTitle().length() < serverasMed.getTitle().length()) return med;
+        return serverasMed;
     }
 
 }
