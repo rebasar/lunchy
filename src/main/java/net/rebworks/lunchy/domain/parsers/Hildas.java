f8d5fd4f9b60e7a4eb41b7c1787002d910e07e63
Mumuksia <yurko.zavada@gmail.com>
2019-10-04 11:14:29 +0200

    Update Hildas.java

--- src/main/java/net/rebworks/lunchy/domain/parsers/Hildas.java
+++ src/main/java/net/rebworks/lunchy/domain/parsers/Hildas.java
@@ -1,5 +1,6 @@
 package net.rebworks.lunchy.domain.parsers;
 
+import java.util.ArrayList;
 import net.rebworks.lunchy.domain.Parser;
 import net.rebworks.lunchy.domain.date.DateCalculator;
 import net.rebworks.lunchy.dto.ImmutableLunch;
@@ -10,6 +11,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import javax.inject.Inject;
@@ -23,55 +25,58 @@ import java.util.stream.Collectors;
 
 public class Hildas implements Parser {
 
-    private final DateCalculator dateCalculator;
+  private final DateCalculator dateCalculator;
 
-    @Inject
-    public Hildas(final DateCalculator dateCalculator) {
-        this.dateCalculator = dateCalculator;
+  @Inject
+  public Hildas(final DateCalculator dateCalculator) {
+    this.dateCalculator = dateCalculator;
+  }
+
+  @Override
+  public List<Lunch> parse(String input) {
+    final Document document = Jsoup.parse(input);
+    final Elements lunchContainers = document.select("div.meny");
+
+    if (lunchContainers.isEmpty()) {
+      return Collections.emptyList();
     }
 
-    @Override
-    public List<Lunch> parse(String input) {
-        final Document document = Jsoup.parse(input);
-        final Elements lunchContainers = document.select("div.slick-track");
+    final Element lunchContainer = lunchContainers.first();
 
-        if (lunchContainers.isEmpty()) return Collections.emptyList();
+    List<Lunch> lunches = new ArrayList<>();
 
-        final Element lunchContainer = lunchContainers.first();
+    List<Node> nodes = lunchContainer.childNodes();
 
-        final Elements days = lunchContainer.select("div.slick-slide");
+    for (int i = 0; i < nodes.size();) {
+      Node item = nodes.get(i);
+      if (item.nodeName().equals("strong")) {
+        i += 2;
+        final DayOfWeek lunchDay = dateCalculator
+            .parseDayOfWeekOrToday(item.nodeName().toLowerCase());
+        String description = ((TextNode) nodes.get(i)).text();
+        List<LunchItem> lunchItemList = new ArrayList<>();
+        i += 2;
+        while (i < nodes.size() && !(nodes.get(i) instanceof Element && ((Element) nodes.get(i))
+            .tagName().equals("strong"))) {
 
-        //days.next()
+          if (!(nodes.get(i) instanceof Element) || !((Element) nodes.get(i)).tagName()
+              .equals("br")) {
+            lunchItemList
+                .add(LunchItem.builder().description(description)
+                    .title(((TextNode) nodes.get(i)).text()).build());
 
-        String[] lunches = lunchContainer.text().split("<strong>");
-
-        return Arrays.stream(lunches).map(this::parseLunch).collect(Collectors.toList());
-    }
-
-    private Lunch parseLunch(final String element) {
-        final DayOfWeek lunchDay = parseLunchDay(element);
-        final LocalDate validFrom = dateCalculator.getDayOfWeek(lunchDay);
-        final ImmutableLunch.Builder lunch = Lunch.builder().validFrom(validFrom).validUntil(validFrom.plusDays(1));
-
-        String lunchItemsString = element.substring(element.indexOf("/>")+1);
-
-        String[] items = lunchItemsString.split("<br />");
-
-        String description = items[0];
-
-        for (int i = 1; i<items.length; i++){
-            if (StringUtils.isNoneEmpty(items[i].trim())){
-                lunch.addItems(LunchItem.builder().title(items[i]).description(description).build());
-            }
-
+          }
+          i++;
         }
+        lunches.add(Lunch.builder().validFrom(dateCalculator.getDayOfWeek(lunchDay))
+            .validUntil(dateCalculator.getDayOfWeek(lunchDay).plusDays(1))
+            .addAllItems(lunchItemList).build());
 
-        return lunch.build();
+
+      }
     }
 
-    private DayOfWeek parseLunchDay(final String element) {
-        System.out.println(element);
-        final String dateString = element.substring(0, element.indexOf("<"));
-        return dateCalculator.parseDayOfWeekOrToday(dateString.toLowerCase());
-    }
+    return lunches;
+  }
+
 }
