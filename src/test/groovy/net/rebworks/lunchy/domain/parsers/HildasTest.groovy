98958f37ce29c6413bb00617bae2171fca929290
Mumuksia <yurko.zavada@gmail.com>
2019-10-07 21:16:23 +0200

    Hildas (#15)

---
+++ src/test/groovy/net/rebworks/lunchy/domain/parsers/HildasTest.groovy
@@ -0,0 +1,22 @@
+package net.rebworks.lunchy.domain.parsers
+
+import net.rebworks.lunchy.domain.date.DateCalculator
+import spock.lang.Specification
+
+import java.time.LocalDate
+
+
+class HildasTest extends Specification {
+    def "Hildas parser parses a valid file successfully"() {
+        given: "A test file with valid content"
+        def contents = this.getClass().getResource("/testData/hildas/valid.json").text
+        and: "A Hildas Parser"
+        def parser = new Hildas(new DateCalculator(LocalDate.now()))
+        when: "The file is parsed"
+        def lunches = parser.parse(contents)
+        then: "The resulting file should contain correct information"
+        lunches.size() == 5
+        lunches.stream().allMatch({ lunch -> lunch.items.size() == 4 })
+    }
+
+}
